cfc702f9659c68963192be23b571f53badbc393e
Mazharrais <mazharmajo72@gmail.com>
2023-11-13 12:05:00 +0500

    add changes

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 function changeBG(color){
     document.body.style.backgroundColor = color;
     var txt = document.getElementsByClassName("text");
+    debugger
     if(color == "#008000"){
         for(let elm of txt){
             elm.style.color = "burlywood";
         }
-    } else{
+        
+    } 
+    else if(color == "#800080"){
+        for(let elm of txt){
+            elm.style.color = "burlywood";
+        }
+    }
+    else{
         for(let elm of txt){
             elm.style.color = "black";
         }
